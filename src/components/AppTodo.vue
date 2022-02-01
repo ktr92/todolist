4cd6b098f9dff2b88c694addf99ac998d4166d74
ktr92 <kudashevtim@gmail.com>
2022-02-01 20:57:19 +0300

    master

---
+++ src/components/AppTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form>
+      <div class="card" v-if="todo">
+        <div class="card-title">
+          {{ todo.name }}
+        </div>
+        <div class="card-list">
+          <div v-for="(item, index) in todo.list" :key="index">
+            {{ item.listname }}
+            <input type="checkbox" :id="item."><label for=""></label>
+          </div>    
+        </div><!-- /.card-list -->
+      </div>
+      <div v-else>
+        <div class="card">Такой заметки нет</div><!-- /.card -->
+      </div>
+    </form>
+   </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    todo: {}
+  }),
+  mounted() {
+    this.todo = this.$store.getters.todo(this.$route.params.id)
+  }
+}
+</script>
+
+<style>
+
+</style>
